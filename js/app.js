e6718de21330850f16a577a4dec72bd30fca4c81
fsibapt <michele.reho@gmail.com>
2015-12-09 23:00:46 +0100

    Initial Build

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ gunlist = document.getElementById('gunlist');
 for(guntype in guns){
     gunlist.innerHTML += '<li data-role="list-divider">'+guns[guntype]['name']+'</li>';
     for(gun in guns[guntype]['guns']){
-        gunlist.innerHTML += '<li><a href="#gun">'+guns[guntype]['guns'][gun]['name']+'</a></li>';
+        gunlist.innerHTML += '<li><a href="#gun" data-transition="slide" onclick="changegun(' + "'" + gun + "', '" + guntype + "'" + ')">'+guns[guntype]['guns'][gun]['name']+'</a></li>';
+    }
+}
+
+function changegun(gun, guntype){
+    sgun = guns[guntype]['guns'][gun];
+    for(index in document.getElementsByClassName('gunname')){
+        document.getElementsByClassName('gunname')[index].innerHTML = sgun['name'];
     }
 }
